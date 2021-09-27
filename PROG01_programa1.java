67bd85ade85718ecf87b6f313adeaae905acbbe2
rfortesperez <90281193+rfortesperez@users.noreply.github.com>
2021-09-27 22:06:31 +0200

    Add files via upload

---
+++ PROG01_programa1.java
@@ -0,0 +1,13 @@
+
+class PROG01_programa1 {
+
+    public static void main(String[] args){
+    System.out.println("Módulo profesional - Programación. UNIDAD DE TRABAJO 01"); // Aquí nos irá mostrando las frases que le indicamos por pantalla
+	System.out.println("Introducción a la programación");
+	System.out.println("Rocío Fortes Pérez");
+	System.out.println("A Coruña - A Coruña");
+	System.out.println("26/09/21");
+	System.out.println("Programa 1");
+    }
+
+}
